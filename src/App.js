1f09cffc6fd2a989764128087cdfbd5e05f55aec
Jung Yun Han <hanjy2008@naver.com>
2022-04-11 17:38:31 +0900

    Coin Tracker_Challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [click, setClick] = useState(true);
+  const [coinName, setCoinName] = useState("");
+  const onChange = (event) => {
+    setBalance(event.target.value);
+  };
+  const onClick = () => {
+    setClick(false);
+  };
+  const onSelect = (event) => {
+    setCoinName(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (balance === "") {
+      return;
+    }
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins{loading ? "" : `(total: ${coins.length}coins)`}</h1>
+      {click ? (
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={onChange}
+            value={balance}
+            type="text"
+            placeholder="input your balance"
+          />
+          <button onClick={onClick}>Deposit</button>
+        </form>
+      ) : (
+        <h2>Your balance: {`${balance} USD`}</h2>
+      )}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelect}>
+            <option>Select coin</option>
+            {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <hr />
+          {`You can buy ${(1 / coinName.split(" ")[2]) * balance}${" "}
+          ${coinName.split(" ")[0]} with your balance.`}
+        </div>
+      )}
     </div>
   );
 }
